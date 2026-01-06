b735c02644616337551071c83ddce24e60232cec
JEFfersusu <142289262+JEFfersusu@users.noreply.github.com>
2026-01-06 11:38:32 +0800

    Update MiT_loss.py

--- MiT_loss.py
+++ MiT_loss.py
@@ -1,18 +1,3 @@
-# class MiT_Loss(nn.Module):
-#     def __init__(self, T=0.5, lambda_entropy=0.1):
-#         super().__init__()
-#         self.ce = nn.CrossEntropyLoss()
-#         self.T = T
-#         self.lambda_entropy = lambda_entropy
-
-#     def forward(self, outputs, targets):
-#         scaled_outputs = outputs / self.T
-#         ce_loss = self.ce(scaled_outputs, targets)
-#         probs = torch.softmax(scaled_outputs, dim=1)
-#         entropy = - (probs * torch.log(probs + 1e-10)).sum(dim=1).mean()
-
-#         return ce_loss - self.lambda_entropy * entropy
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -133,3 +118,4 @@ class MiTLoss_WithTrainCalibration(nn.Module):
             "T": T.detach()
         }
         return loss, stats
+
